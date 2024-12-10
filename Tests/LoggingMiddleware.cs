92d5cf7f36ae4f26a96b8229a11b13e3a3df92d8
DevOnBike <rychter.maciej@gmail.com>
2024-12-10 22:04:04 +0100

    middlewares engine

---
+++ Tests/LoggingMiddleware.cs
@@ -0,0 +1,17 @@
+﻿using Logic.Pipelining;
+
+namespace Tests
+{
+    public class LoggingMiddleware : IMiddleware
+    {
+        public async Task ExecuteAsync(MiddlewareContext middlewareContext, Task next)
+        {
+            Console.WriteLine($"Request: {middlewareContext.Request}");
+            await next;
+            Console.WriteLine($"Response: {middlewareContext.Response}");
+        }
+
+       
+    }
+
+}
---
+++ Tests/ResponseMiddleware.cs
@@ -0,0 +1,15 @@
+﻿using Logic.Pipelining;
+
+namespace Tests
+{
+    public class ResponseMiddleware : IMiddleware
+    {
+        public async Task ExecuteAsync(MiddlewareContext middlewareContext, Task next)
+        {
+            middlewareContext.Response = "Processed by ResponseMiddleware";
+            await next;
+        }
+        
+    }
+
+}
